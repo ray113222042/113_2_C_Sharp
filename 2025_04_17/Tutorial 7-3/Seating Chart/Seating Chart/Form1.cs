1cdd5f2e7dec9186ebc7db49a9aed6f71c408194
郭炘叡 <a113222042@mail.shu.edu.tw>
2025-04-17 12:03:32 +0800

    2025_04_17

---
+++ 2025_04_17/Tutorial 7-3/Seating Chart/Seating Chart/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seating_Chart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void displayPriceButton_Click(object sender, EventArgs e)
+        {
+            const int ROWS = 6;
+            const int COLS = 4;
+            int row;
+            int cols;
+            decimal[,] seatprices = { {450m, 450m, 450m, 450m},
+                                               {425m, 425m, 425m, 425m},
+                                               {400m, 400m, 400m, 400m},
+                                               {375m, 375m, 375m, 375m},
+                                               {375m, 375m, 375m, 375m},
+                                               {350m, 350m, 350m, 350m}
+                                           };
+            if (int.TryParse(rowTextBox.Text, out row))
+            {
+                if (int.TryParse(colTextBox.Text, out cols))
+                {
+                    if (row >= 0 && row < ROWS)
+                    {
+                        if (cols >= 0 && cols < COLS)
+                        {
+                            // 顯示座位價格
+                            priceLabel.Text = seatprices[row, cols].ToString("C");
+                        }
+                        else
+                        {
+                            MessageBox.Show("請輸入有效的列號。");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("請輸入有效的行號。");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("請輸入有效的列號。");
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入有效的行號。");
+            }
+        }
+
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
